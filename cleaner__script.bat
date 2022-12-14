99bb1be59a14e39f9cfc7c24a2fd3c8e01481180
blackmen <105991766+Ryfju@users.noreply.github.com>
2022-12-14 13:44:11 +0100

    Add files via upload
    Rivel bardzo latwy crack src i wszystko w folderze

---
+++ cleaner__script.bat
@@ -0,0 +1,30 @@
+/s /f /q c:\windows\temp\*.*
+rd /s /q c:\windows\temp
+md c:\windows\temp
+del /s /f /q C:\WINDOWS\Prefetch
+del /s /f /q %temp%\*.*
+rd /s /q %temp%
+md %temp%
+deltree /y c:\windows\tempor~1
+deltree /y c:\windows\temp
+deltree /y c:\windows\tmp
+deltree /y c:\windows\ff*.tmp
+deltree /y c:\windows\history
+deltree /y c:\windows\cookies
+deltree /y c:\windows\recent
+deltree /y c:\windows\spool\printers
+del c:\WIN386.SWP
+FOR /F "tokens=1, 2 * " %%V IN ('bcdedit') DO SET adminTest=%%V
+IF (%adminTest%)==(Access) goto noAdmin
+for /F " tokens=*" %%G in ('wevtutil.exe el') DO (call :do_clear "%%G")
+goto theEnd
+:do_clear
+echo clearing %1
+wevtutil.exe cl %1
+goto :eof
+:noAdmin
+echo You must run this script as an Administrator !
+echo ^<press any key^>
+cls
+pause
+exit
